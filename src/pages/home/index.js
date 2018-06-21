a9ef0d4732afa2860910a9f9efea810e26ca0a87
ystoby <yuan.shuai@cgtn.com>
2018-06-21 17:40:11 +0800

    update： 1、export

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -256,4 +256,4 @@ class MyComponent extends React.Component {
 }
 
 ReactDOM.render(<MyComponent /> , document.getElementById('app'));
-export default ProcessIndicator;
+export default MyComponent;
